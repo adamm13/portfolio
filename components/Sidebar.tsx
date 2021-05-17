0608af649919d4755187af37a0346ed57f8b123c
Adam Mohammed <13.adamm@gmail.com>
2021-05-17 15:34:03 -0400

    added tailwind to display columns and rows. added sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div>
+      <img 
+        src="https://avatars.githubusercontent.com/u/43428818?v=4" 
+        alt="user avatar"/>
+        <h3>
+          <span>Adam</span>
+           Mohammed
+        </h3>
+        <p>Web Developer</p>
+        <p>Download Resume</p>
+    </div>
+  )
+}
