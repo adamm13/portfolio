1330e69a015db8a33a1a50ff0ef7288a352dbb14
Adam Mohammed <13.adamm@gmail.com>
2021-05-17 17:08:35 -0400

    finish sidebar and update tailwind colors

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,40 +11,44 @@ export default function Sidebar() {
         alt="user avatar"
         className="w-32 h-32 mx-auto rounded-full"
         />
-        <h3 className="my-4 text-3xl font-medium tracking-wide">
-          <span>Adam</span>
+        <h3 className="my-4 text-3xl font-medium tracking-wider font-Ubuntu">
+          <span className="text-green">Adam </span>
            Mohammed
         </h3>
-        <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">
+        <p className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full">
           Web Developer
           </p>
           <a
-          className="px-2 py-1 my-3 bg-gray-200 rounded-full" 
+          className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full" 
           href="" 
           download="name">
           <GiTie className="w-6 h-6"/>Download Resume
         </a>
-        <div>
+        <div className="flex justify-around my-5 text-blue-500 w-9/12 md:w-full mx-auto">
           <a href="">
-            <AiFillYoutube className="w-8 h-8"/>
+            <AiFillYoutube className="w-8 h-8 cursor-pointer"/>
           </a>
           <a href="">
-            <AiFillGithub className="w-8 h-8"/>
+            <AiFillGithub className="w-8 h-8 cursor-pointer"/>
           </a>
           <a href="">
-            <AiFillLinkedin className="w-8 h-8"/>
+            <AiFillLinkedin className="w-8 h-8 cursor-pointer"/>
           </a>
         </div>
-        <div>
-          <div>
-            <GoLocation/>
+
+        {/* address */}
+        <div className="py-4 my-5 bg-gray-200" style={{ marginLeft: "-1rem", marginRight: "-1rem" }}>
+          <div className="flex items-center justify-center space-x-2">
+            <GoLocation className="mr-2"/>
             <span>Toronto, Canada</span>
           </div>
-          <p>13.adamm@gmail.com</p>
-          <p>416-573-0735</p>
+          <p className="my-2 ">13.adamm@gmail.com</p>
+          <p className="my-2 ">416-573-0735</p>
         </div>
-        <button>Email Me</button>
-        <button>Toggle Theme</button>
+        <button className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2 focus:outline-none"
+        onClick={() => window.open('mailto:13.adamm@gmail.com')}>
+        Email Me</button>
+        <button className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">Toggle Theme</button>
     </div>
   )
 }
