2a135f5d118d0365ec1b0be97fd75343f3380082
Adam Mohammed <13.adamm@gmail.com>
2021-05-19 22:00:53 -0400

    updated projectcard function styling

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,12 +3,12 @@ import { projects } from "../data";
 
 const Projects = () => {
   return (
-    <div className='p-4'>
+    <div className="px-5 py-2 overflow-y-scroll" style={{height: '65vh'}}>
       <nav>Navbar</nav>
 
-      <div className="grid grid-cols-12 gap-4 my-3">
+      <div className="relative grid grid-cols-12 gap-4 my-3">
         {projects.map((project) => (
-            <div className="col-span-12 p-2 sm:col-span-6 lg:col-span-4">
+            <div className="col-span-12 p-2 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-700">
               <ProjectCard project={project} key={project.name}/>
             </div>
         ))}
