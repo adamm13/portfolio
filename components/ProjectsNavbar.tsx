df5523b5d8274577d9f6a2377b7646172598d8b3
Adam Mohammed <13.adamm@gmail.com>
2021-05-20 13:23:41 -0400

    added filter fuction to navbar in projects

---
+++ components/ProjectsNavbar.tsx
@@ -0,0 +1,38 @@
+import { Category } from "../types"
+import { FunctionComponent} from "react"
+
+export const NavItem:FunctionComponent<{
+  value:Category | "all";
+  handlerFilterCategory:Function
+  active:string
+  }> = ({value, handlerFilterCategory, active }) => {
+
+    let className = "cursor-pointer hover:text-green capitalize"
+
+    if(active === value) {
+      className += " text-green"
+    }
+
+  return (
+    <li className={className} onClick={() => handlerFilterCategory(value)}>
+      {value}
+    </li>
+  )
+}
+
+const ProjectsNavbar: FunctionComponent<{handlerFilterCategory:Function, active:string}> = (props) => {
+  return (
+    <div className="flex space-x-3 px-3 py-2 list-none overflow-x-auto">
+      <NavItem value = "all" {...props}/>
+      <NavItem value = "react" {...props}/>
+      <NavItem value = "mongo" {...props}/>
+      <NavItem value = "django" {...props}/>
+      <NavItem value = "node" {...props}/>
+
+    </div>
+  )
+}
+
+export default ProjectsNavbar
+
+
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,35 @@
 import ProjectCard from "../components/ProjectCard";
-import { projects } from "../data";
+import ProjectsNavbar from "../components/ProjectsNavbar";
+import { projects as projectsData } from "../data";
+import {useState} from 'react'
+import { Category } from "../type"
 
 const Projects = () => {
+
+const [projects, setProjects] = useState(projectsData)
+const [active, setActive] = useState("all")
+
+const handlerFilterCategory = (category:Category | "all") => {
+  if(category === "all"){
+    setProjects(projectsData);
+    setActive(category)
+    return;
+  }
+  
+ const newArray =  projectsData.filter(project => 
+ project.category.includes(category)
+ );
+ setProjects(newArray)
+ setActive(category)
+}
+
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{height: '65vh'}}>
-      <nav>Navbar</nav>
+      
+      <ProjectsNavbar handlerFilterCategory={handlerFilterCategory} active={active}/>
 
       <div className="relative grid grid-cols-12 gap-4 my-3">
+
         {projects.map((project) => (
             <div className="col-span-12 p-2 sm:col-span-6 lg:col-span-4 bg-gray-200 dark:bg-dark-700">
               <ProjectCard project={project} key={project.name}/>
